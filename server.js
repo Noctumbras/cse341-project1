9d93da18794c1587c514bfaf7d35de59b702181e
Noctumbras <ranger.stephen@hotmail.com>
2026-09-12 13:10:24 -0500

    finished week 1

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
+
+const mongodb = require('./data/database.js');
 const app = express();
+
+const dns = require("dns");
+dns.setServers(["1.1.1.1"]);
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {console.log(`Running on port ${port}`)});
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {console.log(`Database listening. Node running on port ${port}`)});
+    }
+});
